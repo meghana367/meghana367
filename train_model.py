8083a67a4991a078c88c37599809ca6cc4fc40ca
meghana367 <smeghana021@gmail.com>
2025-04-29 12:00:44 +0530

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,9 @@
+import pickle
+
+# Save the model
+with open('/mnt/data/spam_model.pkl', 'wb') as model_file:
+    pickle.dump(model, model_file)
+
+# Save the vectorizer
+with open('/mnt/data/vectorizer.pkl', 'wb') as vec_file:
+    pickle.dump(vectorizer, vec_file)
